38010715ca305dc4c2ec1223e020a8bb293e9947
Dmytro Medynskyi <48735893+twoSad@users.noreply.github.com>
2021-04-06 13:39:34 +0300

    Update closeMark.js

--- closeMark.js
+++ closeMark.js
@@ -2,7 +2,6 @@ let listElemts = document.getElementsByTagName("li")
 
 for(let i = 0; i < listElemts.length; i++){
     let span = document.createElement("span")
-    //let mark = document.createTextNode("\u00D7")
     let mark = document.createElement("i")
     mark.className = "far fa-trash-alt delete"
 
