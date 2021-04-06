f5f39e00acd18b1252bb13dfe6d19e8102a72974
twoSad <dv.medinskiy@gmail.com>
2021-04-06 13:32:18 +0300

    version 0.1.1
    Change UI:
    -add design fitures
    -align app
    
    Fix bugs:
    -too long input

--- addNew.js
+++ addNew.js
@@ -1,29 +1,36 @@
-function newElement () {
+function newElement() {
     let li = document.createElement("li")
     let inputValue = document.getElementById("newItemValue").value
-    let t = document.createTextNode(inputValue)
+    
+    if (inputValue.length >= 30) {
+        alert("Too long!")
+        document.getElementById("newItemValue").value = ""
+        throw new Error("Too long input");
+    } else {
+        let t = document.createTextNode(inputValue)
+        li.appendChild(t)
+    }
 
-    li.appendChild(t)
-
-    if(inputValue === ""){
+    if (inputValue === "") {
         alert("Empty input!")
-    }else{
+    } else {
         document.getElementById("list").appendChild(li)
     }
 
     document.getElementById("newItemValue").value = ""
 
     let span = document.createElement("span")
-    let closeMark = document.createTextNode("\u00D7")
-    
+    let closeMark = document.createElement("i")
+    closeMark.className = "far fa-trash-alt delete"
+
     span.className = "close"
     span.appendChild(closeMark)
     li.appendChild(span)
 
     let close = document.getElementsByClassName("close");
 
-    for(let i = 0; i < close.length; i++){
-        close[i].onclick = function (){
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
             let div = this.parentElement
             div.style.display = "none"
         }
--- closeMark.js
+++ closeMark.js
@@ -2,7 +2,9 @@ let listElemts = document.getElementsByTagName("li")
 
 for(let i = 0; i < listElemts.length; i++){
     let span = document.createElement("span")
-    let mark = document.createTextNode("\u00D7")
+    //let mark = document.createTextNode("\u00D7")
+    let mark = document.createElement("i")
+    mark.className = "far fa-trash-alt delete"
 
     span.className = "close"
     span.appendChild(mark)
